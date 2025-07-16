5f05ccf765d901e754ee36b4f233880e2649fd92
awrreny <awrreny@gmail.com>
2025-07-16 20:17:36 +0100

    added password policy support, to support requirements by sites (e.g backslash unsupported, need 1 lowercase letter), without affecting deterministic property

--- python-cli-prototype/main.py
+++ python-cli-prototype/main.py
@@ -17,6 +17,25 @@ SETTINGS_FILE = "settings.json"
 with open(SETTINGS_FILE, "r") as f:
     settings = json.load(f)
 
+POLICY_FILE = "pass_policies.json"
+with open(POLICY_FILE, "r") as f:
+    policies = json.load(f)
+
+"""
+there exists a removed policy which contains more characters than the default policy, but may not work with some sites.
+the default policy doesn't contain potentially disallowed characters like `"` or `\`,  or ambiguous characters like `l` and `1`, `O` and `0`,
+but still maintains >90 bits of entropy.
+it was removed to make the tip 'always picking the first applicable policy' less confusing.
+"allsymbols": {
+        "length": 16,
+        "allowedChars": "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
+        "requireUppercase": true,
+        "requireLowercase": true,
+        "requireNumbers": true,
+        "requireSpecialChars": true
+    },
+"""
+
 
 def pos_int_input(prompt):
     while True:
@@ -80,10 +99,26 @@ def get_password():
     else:
         counter = 0
 
+    policy = policies.get("default")
     if settings.get("askPolicy", "yes") == "yes":
-        policy = input("Enter the password policy: ").strip()
-    else:
-        policy = None
+        while True:
+            # lets user choose policy by index or name
+            print("\nAvailable password policies:")
+            for idx, (name, ans) in enumerate(policies.items()):
+                print(f"[{idx}] {name} ({ans['length']} chars)")
+            ans = input("Enter the password policy or leave blank for default. " \
+            "\nTip: always picking the first applicable policy removes the need to remember which one was chosen.").strip()
+            if ans.isdigit() and 0 <= int(ans) < len(policies):
+                policy = list(policies.values())[int(ans)]
+                break
+            elif ans in policies:
+                policy = policies[ans]
+                break
+            elif ans == "":
+                break
+            else:
+                print("Invalid policy, please enter the index or name of the policy")
+                continue   
 
     password = generate_password(master_key, domain, username, counter, policy)
     print(f"Generated password: {password}")
@@ -92,33 +127,62 @@ def get_password():
     # TODO save relevant data (domain, username, counter, policy) if settings allow it
 
 
-def generate_password(master_key, domain: str, username="", counter=0, policy=None):
-    # TODO implement policy handling (e.g., length, allowed characters)
+def generate_password(master_key, domain: str, username, counter, policy):
 
-    password_length = 16  # default password length
-    allowed_chars = settings.get("allowedChars", string.ascii_letters + string.digits + string.punctuation)
-    
-    hash_salt = json.dumps({
-        "domain": domain,
-        "username": username,
-        "counter": counter
-    }).encode('utf-8')
+    password_length = policy.get("length")
+    allowed_chars = policy.get("allowedChars")
+    # for rejection sampling
+    policy_counter = 0
 
 
-    if not argon2_available:
-        hashval = hashlib.pbkdf2_hmac('sha256', master_key, hash_salt, 600000, dklen=password_length)
-    else:
-        hashval = argon2.low_level.hash_secret_raw(master_key, hash_salt, 
-                                                   time_cost=3, memory_cost=65536, parallelism=4,
-                                                   hash_len=password_length, type=argon2.low_level.Type.ID)
-    
-    # conversion by base conversion
-    x = int.from_bytes(hashval)
-    password = []
-    while len(password) < password_length:
-        x, remainder = divmod(x, len(allowed_chars))
-        password.append(allowed_chars[remainder])
+    while True:
+        hash_salt = json.dumps({
+            "domain": domain,
+            "username": username,
+            "counter": counter,
+            "counter2": policy_counter,
+        }).encode('utf-8')
+        
+        if not argon2_available:
+            hashval = hashlib.pbkdf2_hmac('sha256', master_key, hash_salt, 600000, dklen=password_length)
+        else:
+            hashval = argon2.low_level.hash_secret_raw(master_key, hash_salt, 
+                                                        time_cost=3, memory_cost=65536, parallelism=4,
+                                                        hash_len=password_length, type=argon2.low_level.Type.ID)
+        # conversion by base conversion
+        x = int.from_bytes(hashval)
+        password = []
+        while len(password) < password_length:
+            x, remainder = divmod(x, len(allowed_chars))
+            password.append(allowed_chars[remainder])
+        if verify_policy(policy, password):
+            break
+
+        policy_counter += 1
+        if policy_counter > 100:
+            raise ValueError("Failed to generate a valid password after 1000 attempts. Please check the policy settings.")
+        
     return ''.join(password)
+
+
+def verify_policy(policy, password):
+    """
+    Verifies if the password meets the policy requirements.
+    Does not check length or character set as those are already enforced by the password generation.
+    """
+    if policy.get("requireUppercase") and not any(c.isupper() for c in password):
+        return False
+    
+    if policy.get("requireLowercase") and not any(c.islower() for c in password):
+        return False
+    
+    if policy.get("requireNumbers") and not any(c.isdigit() for c in password):
+        return False
+    
+    if policy.get("requireSpecialChars") and not any(c in string.punctuation for c in password):
+        return False
+    
+    return True
 
 
 def change_authentication_methods():
