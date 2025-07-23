3d59a0f266070435219e993605a303933063bcda
awrreny <awrreny@gmail.com>
2025-07-23 16:54:55 +0100

    added a variant of secret sharing to crypto_primitives.py

--- python-cli-prototype/crypto_primitives.py
+++ python-cli-prototype/crypto_primitives.py
@@ -1,8 +1,30 @@
 import hashlib
+from shamir_secret_sharing import SplitSecretSharer
 try:
     import argon2
 except ImportError:
     argon2 = None
+
+
+# variant of secret sharing where knowing t shares does not leak the other n-t shares
+# input shares should have high entropy
+class NoLeakSecretSharer:
+    def __init__(self, shares: list[bytes], key_threshold: int, secret_byte_len: int = 32):
+        self.byte_len = secret_byte_len
+        encoded_shares = [
+            fast_hash(share, secret_byte_len)
+            for share in shares
+        ]
+        self.internal_sharer = SplitSecretSharer(encoded_shares, key_threshold)
+    
+
+    def get_secret(self, known_shares: dict[int, bytes]):
+        known_encoded_shares = {
+            i: fast_hash(known_share, self.byte_len)
+            for i, known_share in known_shares.keys()
+        }
+
+        return self.internal_sharer.get_secret(known_encoded_shares)
 
 
 def fast_hash(inp: bytes, byte_len=32):
