da68c9fe70ad55d3ad0928850b54e879a94cb5df
awrreny <awrreny@gmail.com>
2025-07-30 20:50:00 +0100

    fixed get_node_secret and added create_tree_and_return_secret

--- python-cli-prototype/treefa.py
+++ python-cli-prototype/treefa.py
@@ -2,10 +2,15 @@
 # zero-knowledge tree thing that only gives the 'root secret' if all conditions are met
 # this zero-knowledge is enforced cryptographically rather than by a server
 # e.g for local password managers
+
+# TODO add OrNode and AndNode as wrappers for AnyTNode with t=1 and t=n 
+import pickle
 from dataclasses import dataclass
 from inpututil import get_input, choose_option
 from crypto_primitives import slow_hash, fast_hash, NoLeakSecretSharer
+from getpass import getpass
 
+TREE_FILE = "treefa_tree.pkl"
 
 @dataclass(frozen=True)
 class AuthNode:
@@ -51,7 +56,7 @@ def get_node_secret(node: AuthNode):
     match node:
         case PasswordNode(verification_hash=verification_hash, name=name):
             while True:
-                password = get_input(f"Enter password for node '{name}'\n> ")
+                password = getpass(f"Enter password for node {name} (input will be hidden):\n> ")
                 # there are 2 hashes here - the first is more of an encoding or an extending
                 # the 2nd is to check if the correct password was entered, but is only for usability (to notify user if password is wrong)
                 # it has no cryptographic role in terms of getting the node secret (or any other secret)
@@ -62,7 +67,7 @@ def get_node_secret(node: AuthNode):
                     print("Incorrect password")
 
 
-        case AnyTNode(verification_hash=verification_hash, threshold=threshold, children=children, sharer_object=sharer_object):
+        case AnyTNode(verification_hash=verification_hash, threshold=threshold, children=children, sharer_object=sharer_object, name=name):
             known_shares = dict()
             auth_options = {
                 i: child.name
@@ -71,7 +76,7 @@ def get_node_secret(node: AuthNode):
             while threshold > 0:
                 node_index = choose_option(auth_options,
                                             text1=f"Select which method to authenticate with. ({threshold} remaining)",
-                                            inp_type=int,
+                                            inp_type=int, # type: ignore
                                             )
                 known_shares[node_index] = get_node_secret(children[node_index])
                 auth_options.pop(node_index)
@@ -81,6 +86,11 @@ def get_node_secret(node: AuthNode):
 
             if fast_hash(secret) != verification_hash:
                 raise ValueError(f"Node '{name}' failed verification")
+            
+            return secret
+        
+        case _:
+            raise NotImplementedError()
 
         
             
@@ -94,3 +104,75 @@ def get_node_secret(node: AuthNode):
 def get_master_key():
     # placeholder
     return get_node_secret(testnode("placeholder"))
+
+
+def save_tree(obj):
+    with open(TREE_FILE, 'wb') as f:
+        pickle.dump(obj, f)
+
+
+def load_tree():
+    with open(TREE_FILE, 'rb') as f:
+        return pickle.load(f)
+    
+
+def create_tree_and_return_secret() -> tuple[AuthNode, bytes]:
+    node_types = {
+        "p": "Password Node",
+        "t": "Threshold Node (authenticate any t of its children)"
+    }
+    match choose_option(node_types):
+        case "p":
+            node_name = get_input("Enter name for this node:\n> ")
+            while True:
+                password = getpass("Enter password (input will be hidden):\n> ")
+                confirm_pass = getpass("Enter again:\n> ")
+                if password == confirm_pass: break
+                print("Passwords didn't match")
+            secret = slow_hash(password.encode())
+            verification_hash = fast_hash(secret)
+            return PasswordNode(verification_hash, node_name), secret
+        
+        case "t":
+            node_name = get_input("Enter name for this node:\n> ")
+            # currently have n children
+            # add more or finish?
+            # prevent empty list (and singleton?)
+            children: list[AuthNode] = []
+            shares: list[bytes] = []
+            while True:
+                print(f"Adding child {len(children)+1} of {node_name}:")
+                child, share = create_tree_and_return_secret()
+                children.append(child)
+                shares.append(share)
+
+                if len(children) >= 2 and choose_option({
+                    "a": "Add another child node",
+                    "f": "Finished adding children"
+                }) == "f":
+                    break
+            print("Child nodes: ")
+            print("\n".join(child.name for child in children))
+            threshold = get_input(f"Enter threshold number of child nodes to verify this node. " \
+                                  f"e.g enter 1 to require any child node, or {len(children)} to require all:\n> ",
+                                  int, range(1, len(children)+1)) # type: ignore
+
+            sharer = NoLeakSecretSharer(shares, threshold)
+            secret = sharer.get_secret({
+                i: share
+                for i, share in enumerate(shares)
+            })
+            return AnyTNode(verification_hash=fast_hash(secret),
+                            name=node_name,
+                            threshold=threshold,
+                            children=children,
+                            sharer_object=sharer), secret
+        case _:
+            raise ValueError
+        
+
+# nd, sc = create_tree_and_return_secret()
+# print("---")
+# assert get_node_secret(nd) == sc
+
+
